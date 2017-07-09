f55955ea2266790dee2463618b3a9fabe6ea301b
AdityaShane <adityasen_soumitra@srmuniv.edu.in>
2017-07-09 15:15:53 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{int num;
+	System.out.println("enter the number");
+	Scanner s=new Scanner(System.in);
+	num=s.nextInt();
+	if(num%2==0){System.out.println("no is even");}
+	else if(num%2!=0){System.out.println("no is odd");}
+	
+		
+	}
