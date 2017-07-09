b0e47874b4c51151a497549252df516514a39887
AdityaShane <adityasen_soumitra@srmuniv.edu.in>
2017-07-09 15:46:33 +0530

    Add files via upload

---
+++ charornumber.java
@@ -0,0 +1,9 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{char ch;
+	System.out.println("enter the character");
+	Scanner s=new Scanner(System.in);
+	ch=s.next().charAt(0);
+	if(ch>='a'&&ch<='z'){System.out.println("input is char");}
+	else{System.out.println("input is number");}
