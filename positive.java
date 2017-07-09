96af8132a44873ccfabf48aabcbef9a31d74f9d0
AdityaShane <adityasen_soumitra@srmuniv.edu.in>
2017-07-09 15:05:57 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{int num;
+	System.out.println("enter the number");
+	Scanner s=new Scanner(System.in);
+	num=s.nextInt();
+	if(num==0){System.out.println("no is zero");}
+	else if(num>0){System.out.println("no is positive");}
+	else if(num<0){System.out.println("no is negative");}
+		
+	}
+}
